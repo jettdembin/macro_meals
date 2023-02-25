b9a10cc2942eb8545ec23d14da8b9656d31fa5df
Jett <jd.dev96@gmail.com>
2023-02-24 22:00:46 -0500

    successfuly added tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "./page.module.css";
+import { Button } from "@/components/Elements/Button/Button";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-	return <h1 className="text-3xl font-bold underline">Hello worlds!</h1>;
+	return (
+		<h1 className="text-3xl font-bold underline">
+			<Button>Hello worlds!</Button>
+			<button className="px-2 border border-red-800">Test</button>
+			<div className="flex items-center">
+				<div>test</div>
+				<div>test</div>
+				<div>test</div>
+			</div>
+		</h1>
+	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: [
+		"./app/**/*.{js,ts,jsx,tsx}",
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+
+		"./src/app/**/*.{js,ts,jsx,tsx}",
+		"./src/pages/**/*.{js,ts,jsx,tsx}",
+		"./src/components/**/*.{js,ts,jsx,tsx}",
+
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./app/**/*.{js,ts,jsx,tsx}",
+
+		// Or if using `src` directory:
+		"./src/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
